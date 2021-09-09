cf549fecb091d52b05c8978f61826ad5d7fb702e
Messias <juubibiijuu@gmail.com>
2021-09-08 21:46:44 -0300

    Basic statics components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Therodin" target="_blank" rel="noreferrer">Gabriel Messias</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Refeito por Gabriel Messias na semana DevSuperior: <a href="https://www.linkedin.com/in/gabriel-messias-80a29b215/" target="_blank" rel="noreferrer">@lokstargaming</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
